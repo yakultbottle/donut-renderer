eba8d23bfed23941fd4d22a10e3715868933479e
yakultbottle <benjaminlwm7983@gmail.com>
2026-04-16 17:50:40 +0200

    Implement drawing faces without illumination

--- cube.c
+++ cube.c
@@ -1,5 +1,6 @@
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
@@ -7,11 +8,14 @@
 #define HEIGHT 22
 
 #define HALF_CUBE_LEN 1.5
-#define dz 5
-#define ZOOM (HEIGHT * dz * 3) / (8 * HALF_CUBE_LEN * 2)
+#define z_offset 5
+#define ZOOM (HEIGHT * z_offset * 3) / (8 * HALF_CUBE_LEN * 2)
 
 #define dA 0.04
 #define dB 0.02
+// estimated when nearest cube face is to screen, it is 24 pixels long. Found
+// out empirically. 1 / 24 == 0.041, so 0.03 should be fine
+#define dC 0.03
 
 #define LUMINANCE ".,-~:;=!*#$@"
 
@@ -44,6 +48,25 @@ int in_bounds(point2d pix) {
     return pix.x > 0 && pix.x < WIDTH && pix.y > 0 && pix.y < HEIGHT;
 }
 
+point3d* rotate(float A, float B, point3d vertices[]) {
+    float cosA = cos(A);
+    float cosB = cos(B);
+    float sinA = sin(A);
+    float sinB = sin(B);
+
+    point3d *rotated_vertices = malloc(8 * sizeof(point3d));
+
+    for (int i = 0; i < 8; ++i) {
+        rotated_vertices[i] = (point3d){
+            vertices[i].x * cosA + vertices[i].y * sinA,
+            vertices[i].z * sinB + cosB * (vertices[i].y * cosA - vertices[i].x * sinA),
+            vertices[i].z * cosB - sinB * (vertices[i].y * cosA - vertices[i].x * sinA),
+        };
+    }
+
+    return rotated_vertices;
+}
+
 point3d vertices[8] = {
     (point3d){HALF_CUBE_LEN, HALF_CUBE_LEN, HALF_CUBE_LEN},
     (point3d){-HALF_CUBE_LEN, HALF_CUBE_LEN, HALF_CUBE_LEN},
@@ -55,41 +78,57 @@ point3d vertices[8] = {
     (point3d){-HALF_CUBE_LEN, -HALF_CUBE_LEN, -HALF_CUBE_LEN},
 };
 
+// Order is botleft, topright, botleft
+int faces[6][4] = {
+    {1, 0, 3}, // Front face  (Z =  HALF_CUBE_LEN)
+    {5, 4, 7}, // Back face   (Z = -HALF_CUBE_LEN)
+    {1, 0, 5}, // Top face    (Y =  HALF_CUBE_LEN)
+    {3, 2, 7}, // Bottom face (Y = -HALF_CUBE_LEN)
+    {0, 4, 2}, // Right face  (X =  HALF_CUBE_LEN)
+    {5, 1, 7}  // Left face   (X = -HALF_CUBE_LEN)
+};
+
 // Drawing cube at rotation A, B, where A is around X axis and B around Z
 // https://www.symbolab.com/solver/matrix-multiply-calculator/%5Cbegin%7Bpmatrix%7Dx%26y%26z%5Cend%7Bpmatrix%7D%5Cbegin%7Bpmatrix%7Dcos%5Cleft(A%5Cright)%26-sin%5Cleft(A%5Cright)%260%5C%5C%20%20%20sin%5Cleft(A%5Cright)%26cos%5Cleft(A%5Cright)%260%5C%5C%20%20%200%260%261%5Cend%7Bpmatrix%7D%5Cbegin%7Bpmatrix%7D1%260%260%5C%5C%20%20%20%20%20%200%26cos%5Cleft(B%5Cright)%26-sin%5Cleft(B%5Cright)%5C%5C%20%20%20%20%20%200%26sin%5Cleft(B%5Cright)%26cos%5Cleft(B%5Cright)%5Cend%7Bpmatrix%7D?or=input
-void draw_cube(float A, float B, char buf[HEIGHT][WIDTH], float z[HEIGHT][WIDTH]) {
-    float cosA = cos(A);
-    float cosB = cos(B);
-    float sinA = sin(A);
-    float sinB = sin(B);
+void draw_cube(float A, float B, char buf[HEIGHT][WIDTH], float z_buf[HEIGHT][WIDTH]) {
+    point3d *rot_vertices = rotate(A, B, vertices);
 
-    for (int i = 0; i < 8; ++i) {
-        point3d pix3d = {
-            vertices[i].x * cosA + vertices[i].y * sinA,
-            vertices[i].z * sinB + cosB * (vertices[i].y * cosA - vertices[i].x * sinA),
-            vertices[i].z * cosB - sinB * (vertices[i].y * cosA - vertices[i].x * sinA),
-        };
+    for (int face = 0; face < 6; ++face) {
+        point3d TL = rot_vertices[faces[face][0]];
+        point3d TR = rot_vertices[faces[face][1]];
+        point3d BL = rot_vertices[faces[face][2]];
 
-        float inverse_z = 1. / (pix3d.z + dz);
+        for (float u = 0.; u <= 1.0; u += dC) {
+            for (float v = 0.; v <= 1.0; v += dC) {
+                float x = TL.x + (TR.x - TL.x) * u + (BL.x - TL.x) * v;
+                float y = TL.y + (TR.y - TL.y) * u + (BL.y - TL.y) * v;
+                float z = TL.z + (TR.z - TL.z) * u + (BL.z - TL.z) * v;
 
-        point2d pix2d = (point2d){
-            WIDTH / 2 + (int)(pix3d.x * ZOOM * 2 * inverse_z),
-            HEIGHT / 2 - (int)(pix3d.y * ZOOM * inverse_z),
-        };
+                float inverse_z = 1. / (z + z_offset);
+                point2d pix2d = (point2d){
+                    WIDTH / 2 + (int)(x * ZOOM * 2 * inverse_z),
+                    HEIGHT / 2 - (int)(y * ZOOM * inverse_z),
+                };
 
-        // inverse_z being smaller means it is closer to screen, render this pixel
-        if (in_bounds(pix2d) && inverse_z > z[pix2d.y][pix2d.x]) {
-            // buf[pix2d.y][pix2d.x] = '#';
+                // inverse_z being smaller means it is closer to screen, render this pixel
+                if (in_bounds(pix2d) && inverse_z > z_buf[pix2d.y][pix2d.x]) {
+                    buf[pix2d.y][pix2d.x] = '#';
 
-            // Luminance derivation
-            // float L = sinX * cosA * cosB + cosX * sinY * sinA * cosB - cosX * cosY * sinB - cosX * sinYcosA + sinX * sinA;
+                    // Luminance derivation
+                    // float L = sinX * cosA * cosB + cosX * sinY * sinA *
+                    // cosB - cosX * cosY * sinB - cosX * sinYcosA + sinX *
+                    // sinA;
 
-            // z[pix2d.y][pix2d.x] = inverse_z;
-            // luminance equation did not normalise light dir, so factor that in here:
-            // 8 * sqrt(2) = 11.3, now in range 0..11 after int truncation
-            // also clamp to 0, so that all dark parts of the donut are rendered at funny angles
-            // int luminance_index = L > 0 ? (int)(L * 8) : 0;
-            // buf[pix2d.y][pix2d.x] = LUMINANCE[luminance_index];
+                    // z_buf[pix2d.y][pix2d.x] = inverse_z;
+                    // luminance equation did not normalise light dir, so
+                    // factor that in here: 8 * sqrt(2) = 11.3, now in range
+                    // 0..11 after int truncation also clamp to 0, so that
+                    // all dark parts of the donut are rendered at funny
+                    // angles int luminance_index = L > 0 ? (int)(L * 8) :
+                    // 0; buf[pix2d.y][pix2d.x] =
+                    // LUMINANCE[luminance_index];
+                }
+            }
         }
     }
 }
@@ -98,15 +137,15 @@ int main() {
     printf("\x1b[2J"); // clear screen
 
     char buf[HEIGHT][WIDTH];
-    float z[HEIGHT][WIDTH];
+    float z_buf[HEIGHT][WIDTH];
 
     float A = 0.;
     float B = 0.;
 
     while (1) {
         memset(buf, ' ', sizeof(buf));
-        memset(z, 0, sizeof(z));
-        draw_cube(A, B, buf, z);
+        memset(z_buf, 0, sizeof(z_buf));
+        draw_cube(A, B, buf, z_buf);
 
         render_buf(buf);
         usleep(quanta);
